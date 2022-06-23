83ddac2dc511ae748178e0cfd6b8329cc0f37cb9
Sarah Druyts <sarah.druyts@gmail.com>
2022-06-23 18:51:40 +0200

    Issue ID Reindexing
    See ArcheDetector repo for more info
    Also, with the newest index, you can now search on tags

--- src/components/reindex/Reindex.vue
+++ src/components/reindex/Reindex.vue
@@ -3,12 +3,34 @@
 	<standard-navbar/>
 	<b-card header-tag="header" class="mt-5">
 		<template #header>
-			<h5>Reindex the database?</h5>
+			<h5>Reindex the entire database</h5>
 		</template>
 		<div class="overflow-auto justify-content-center">
 			<b-button pill size="sm" variant="danger" class="mb-1" @click="regenerateIndexIssues()">
-				Regenerate Index for Issue Lists
+				Regenerate Issue List Database Index
 			</b-button>
+		</div>
+	</b-card>
+	<b-card header-tag="header" class="mt-5">
+		<template #header>
+			<h5>Reindex a set of issue IDs</h5>
+		</template>
+		<div class="overflow-auto justify-content-center">
+			<p>
+				Please enter the issues you wish to reindex, separated by a comma.
+			</p>
+			<b-row class="d-flex pb-4">
+				<b-col align-self="center">
+					<b-input-group class="ms-1 ">
+						<b-form-input id="issues-csv" v-model="issueList" placeholder="TAG-1234,TAG-5678"></b-form-input>
+					</b-input-group>
+				</b-col>
+				<b-col>
+					<b-button pill size="sm" variant="danger" class="mb-1" @click="regenerateIndexIssuesCSV()">
+						Reindex for these IDs
+					</b-button>
+				</b-col>
+			</b-row>
 		</div>
 	</b-card>
 	</div>
@@ -32,10 +54,20 @@ export default {
 					this.$router.push({name: 'Home'})
 				})
 			}
+		},
+		regenerateIndexIssuesCSV(){
+			let issues = []
+			this.issueList.split(',').forEach(id => {
+				issues.push(id.trim())
+			})
+			// console.log(issues)
+			axios.post(url + "issue/reindex", issues).then(() => {
+					this.$router.push({name: 'Home'})
+			})
 		}
 	}
 }
 </script>
 
-<style>
+<style scoped>
 </style>
