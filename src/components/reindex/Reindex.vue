7ef13988c8da5d29996773cd5e524d55696908e3
Sarah Druyts <sarah.druyts@gmail.com>
2022-07-02 00:09:30 +0200

    import csv for tags

--- src/components/reindex/Reindex.vue
+++ src/components/reindex/Reindex.vue
@@ -33,6 +33,31 @@
 			</b-row>
 		</div>
 	</b-card>
+	<b-card header-tag="header" class="mt-5">
+		<template #header>
+			<h5>Upload classification</h5>
+		</template>
+		<div class="overflow-auto justify-content-center">
+			<p>
+				Upload a .csv file, fill in the custom tag (leave blank for none) and press the button to mass-classify issues.
+			</p>
+			<b-row class="d-flex pb-4">
+				<b-col>
+					<input type="file" class="input-file" @change="previewFiles" accept="text/csv">
+				</b-col>
+				<b-col>
+					<b-input-group class="ms-1 ">
+						<b-form-input id="classific" v-model="classific_tag" placeholder="e.g. MAVEN"></b-form-input>
+					</b-input-group>
+				</b-col>
+				<b-col>
+					<b-button pill size="sm" variant="danger" class="mb-1" @click="uploadClassification()">
+						Upload this classification
+					</b-button>
+				</b-col>
+			</b-row>
+		</div>
+	</b-card>
 	</div>
 </template>
 
@@ -60,10 +85,29 @@ export default {
 			this.issueList.split(',').forEach(id => {
 				issues.push(id.trim())
 			})
-			// console.log(issues)
 			axios.post(url + "issue/reindex", issues).then(() => {
 					this.$router.push({name: 'Home'})
 			})
+		},
+		uploadClassification(){
+			console.log(this.classific_tag)
+			console.log(this.file)
+			const formData = new FormData();
+			formData.append("file", this.file)
+			formData.append("newTag", this.classific_tag)
+			axios.post(url + "tag/upload", formData, {
+				headers: {
+					'Content-Type': 'multipart/form-data'
+				}
+			}).then(() => {
+				this.$router.push({name: 'Home'})
+			}).catch((err) => {
+				console.log(err)
+			})
+		},
+		previewFiles(event){
+			console.log(event.target.files);
+			this.file = event.target.files[0]
 		}
 	}
 }
